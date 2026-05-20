31d59d3fe46a69dddfee29520501f9db814bae4a
AleWu <77410271+TheAleWu@users.noreply.github.com>
2026-05-20 15:55:05 +0200

    Created remaining base microservices for the server-side

---
+++ services/note-service/src/test/java/de/tum/devopss26/noteservice/NoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tum.devopss26.noteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
