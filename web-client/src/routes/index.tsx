272fc8ed9e1705196f4033b69e2f6a5680c24b39
w-richter <werner.richter@seznam.cz>
2026-05-21 20:51:04 +0200

    test push

--- web-client/src/routes/index.tsx
+++ web-client/src/routes/index.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute('/')({ component: Home });
 function Home() {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to our App</h1>
+      <h1 className="text-4xl font-bold">Welcome to our App!</h1>
       <p className="mt-4 text-lg">
           <Button> example button </Button>
       </p>
