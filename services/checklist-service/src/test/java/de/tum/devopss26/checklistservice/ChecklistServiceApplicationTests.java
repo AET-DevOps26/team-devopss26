22d321d4af7093d2e0ddc6867a475fa76272ebb8
AleWu <77410271+TheAleWu@users.noreply.github.com>
2026-07-14 22:59:22 +0200

    implements #96: improvements after PR

--- services/checklist-service/src/test/java/de/tum/devopss26/checklistservice/ChecklistServiceApplicationTests.java
+++ services/checklist-service/src/test/java/de/tum/devopss26/checklistservice/ChecklistServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package de.tum.devopss26.checklistservice;
 
+import de.tum.devopss26.shared.it.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 
 class ChecklistServiceApplicationTests extends AbstractIntegrationTest {
--- services/note-service/src/test/java/de/tum/devopss26/noteservice/NoteServiceApplicationTests.java
+++ services/note-service/src/test/java/de/tum/devopss26/noteservice/NoteServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package de.tum.devopss26.noteservice;
 
+import de.tum.devopss26.shared.it.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 
 class NoteServiceApplicationTests extends AbstractIntegrationTest {
